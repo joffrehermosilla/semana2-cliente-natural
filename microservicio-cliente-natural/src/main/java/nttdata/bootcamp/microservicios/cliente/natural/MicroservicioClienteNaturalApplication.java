fa92198cd153deaf6759951dc0235aac711a6019
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 10:54:48 -0500

    primer commit

---
+++ microservicio-cliente-natural/src/main/java/nttdata/bootcamp/microservicios/cliente/natural/MicroservicioClienteNaturalApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.cliente.natural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioClienteNaturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioClienteNaturalApplication.class, args);
+	}
+
+}
